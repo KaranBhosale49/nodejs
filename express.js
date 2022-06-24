2ac71b0f8c3aa12b844dd9831156f0bab7f86135
Sampat Bhosale <karan.sampat-bhosale@capgemini.com>
2022-06-24 05:36:58 +0530

    24-06-2022

--- express.js
+++ express.js
@@ -1,14 +1,16 @@
 var express=require('express');
 var app = express();
 app.set('view engine','ejs');
+
+
 app.get('/',function(req,res){
-res.send('this is by using app.get ');
+res.render('index')
 });
-app.get('/api',(req,res)=>{
-    res.sendFile(__dirname+'/home.html');
+app.get('/contact',(req,res)=>{
+    res.render('contact')
 })
 app.get('/profile/:name',function(req,res){
-    var data ={age:24, job:'ninja'};
+    var data ={age:24, job:'ninja',hobbies:['eating','fishing','running']};
     res.render('profile',{person: req.params.name,data: data});
 })
 app.listen(3000);
