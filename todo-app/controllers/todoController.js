4d2de975e62bf0332537bb1b96ad38f6a27d68fb
Sampat Bhosale <karan.sampat-bhosale@capgemini.com>
2022-06-25 12:11:12 +0530

    12:15 25-06-22

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,7 +1,8 @@
+var data =[{item:'get milk'},{item:'walk dog'},{item:'do coding'}];
 module.exports=function(app){
 
 app.get('/todo',function(req,res){
-res.render('todo');
+res.render('todo',{todos:data});
 });
 app.post('/todo',function(req,res){
 
