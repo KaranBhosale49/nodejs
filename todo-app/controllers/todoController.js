688b72a33586e37e2f44abeff2b2ef410538c203
Sampat Bhosale <karan.sampat-bhosale@capgemini.com>
2022-06-25 15:44:12 +0530

    3:30 25-06-2022

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,12 +1,19 @@
+var bodyParser = require('body-parser');
+var urlencodedParser= bodyParser.urlencoded({extends:false});
 var data =[{item:'get milk'},{item:'walk dog'},{item:'do coding'}];
 module.exports=function(app){
 
 app.get('/todo',function(req,res){
 res.render('todo',{todos:data});
 });
-app.post('/todo',function(req,res){
 
+
+app.post('/todo',urlencodedParser,function(req,res){
+data.push(req.body);
+res.json(data);
 });
+
+
 app.delete('/todo',function(req,res){
 
 });
