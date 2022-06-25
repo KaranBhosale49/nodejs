27b48397c8c4488eb893e0c2f517a941eb85e42a
Sampat Bhosale <karan.sampat-bhosale@capgemini.com>
2022-06-25 17:03:22 +0530

    5:05 25-06-2022

---
+++ mongoose/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app=express();
+var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
+var Book=require('./Book.model')
+var port=8080;
+
+var db='mongodb://localhost/example';
+mongoose.connect(db);
+app.get('/',function(req,res){
+    res.send('happy to be here');
+});
+
+app.get('/books',function(req,res){
+    console.log('getting books');
+    Book.find({})
+    .exec(function(err,books){
+     if(err){
+        res.send('error occured');
+     }else{
+        console.log(books);
+        res.json(books);
+     }
+    })
+});
+app.listen(port,function(){
+    console.log('app listening on port'+port);
+});
