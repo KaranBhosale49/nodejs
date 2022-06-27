6023b83b9329130f5b54d965caee2883bef35891
Sampat Bhosale <karan.sampat-bhosale@capgemini.com>
2022-06-27 06:15:08 +0530

    commit

--- mongoose/app.js
+++ mongoose/app.js
@@ -58,6 +58,45 @@ app.post('/book',function(req,res){
         }
     })
 })
+app.post('/book2',function(req ,res){
+    Book.creat(req.body,function(err,body){
+        if(err){
+            res.send('error');
+        }else{
+            console.log(book)
+            res.send(book);
+        }
+    });
+});
+app.put('book/:id',function(req,res){
+    Book.findOneAndUpdate({
+        _id:req.params.id
+    },{$set:{title:req.body.title}},
+        {upsert:true},
+        function(err,newBook){
+       if(err){
+        console.log('error occured');
+       } 
+       else{
+        console.log(newBook);
+        res.send(newBook);
+       }
+    })
+});
+app.delete('/book/:id',function(req,res){
+    Book.findOneAndRemove({
+        _id:req.params.id
+    },function(err,book){
+        if(err){
+            res.send('error deleting ');
+        }
+        else{
+            console.log(book)
+            res.status(204);
+        }
+    })
+})
+
 app.listen(port,function(){
     console.log('app listening on port'+port);
 });
